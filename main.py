9d1b80555351a3ae1b80e5ce38e54f23acda1486
Franklin <magolin.franklin@gmail.com>
2018-10-30 16:13:09 -0500

    Finish web-caesar

--- main.py
+++ main.py
@@ -1,66 +1,50 @@
-from flask import Flask, request
+from flask import Flask,request
 from caesar import rotate_string
 
-
 app=Flask(__name__)
-app.config['DEBUG'] = True
-
-form = """
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
 
 
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
+form="""
+<head>
+<style>
+.layout{{
+    margin:0 auto;
+    width:30%;
+    background-color:#EEEEEE;
+    padding:20px;
+    border-radius:10px
+}}
+textarea{{
+    margin:10px 0;
+    width:400px;
+    height:100px;
 
-        </style>
-    </head>
-    <body>
-    <div class='layout'>
-      #<!-- create your form here -->
-      #form uses the post method
-      <form methods="post">
-      <label for="rot">Rotate by:</label>
-      #label on the input element
-            <input type= "text" name = "rot" value = "0">
-            
-            #input element above has a default value of 0
-            #per assignment instructions
-        
-        <textarea type = "text" name = "text">{0}</textarea>
-        #second input per the instrux for the body of HTML string
-        <input type = "submit">
-        </form>
-        </div>
-    
+}}
+.error{{
+color:red;
+}}
+</style>
+</head>
+<body>
+<div class='layout'>
+<form method="post">
+<label for="rot">Rotate by:</label>
+<input type="text" name="rot" value="0">
+<br>
+<p class='error'>{0}</p>
+<br>
+<textarea type="text" name="text">{1}</textarea>
+<br>
+<input type="submit">
 
-
-    </body>
-</html>
-
-
+</form>
+</div>
+</body>
 """
-#wrapped and unwrapped h1 tags to test
-#processing the form
-@app.route("/")
-def index():
-    encrypted_text= ''
-    return form.format(",")
 
+@app.route('/')
+def index():
+    return form.format('','')
 @app.route('/',methods=['POST'])
 def encrypt():
     rot=request.form['rot']
@@ -69,8 +53,7 @@ def encrypt():
         return form.format('Error in rotation value','')
     encrypted_text=rotate_string(text,int(rot))
     return form.format('',encrypted_text)
+
+
 if __name__=='__main__':
-
-    app.run(debug=True)    
-
-
+    app.run(debug=True)
