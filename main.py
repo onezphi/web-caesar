e03b050830dc5bac13bcdbe229fae03f8c47fe04
Franklin <magolin.franklin@gmail.com>
2018-10-30 16:27:00 -0500

    Finish web-caesar

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
-from flask import Flask,request
+from flask import Flask, request
 from caesar import rotate_string
+
 
 app=Flask(__name__)
 
+form = """
+<!DOCTYPE html>
 
-form="""
-<head>
-<style>
-.layout{{
-    margin:0 auto;
-    width:30%;
-    background-color:#EEEEEE;
-    padding:20px;
-    border-radius:10px
-}}
-textarea{{
-    margin:10px 0;
-    width:400px;
-    height:100px;
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
 
-}}
-.error{{
-color:red;
-}}
-</style>
-</head>
-<body>
-<div class='layout'>
-<form method="post">
-<label for="rot">Rotate by:</label>
-<input type="text" name="rot" value="0">
-<br>
-<p class='error'>{0}</p>
-<br>
-<textarea type="text" name="text">{1}</textarea>
-<br>
-<input type="submit">
+        </style>
+    </head>
+    <body>
+    <div class='layout'>
+    <form method="post">
+    <label for="rot">Rotate by:</label>
+    <input type= "text" name = "rot" value = "0">
+    <p class='error'>{0}</p>       
+           
+    <textarea type = "text" name = "text">{1}</textarea>
+       
+        <input type = "submit">
+        </form>
+        </div>
+   
+    </body>
+</html>
 
-</form>
-</div>
-</body>
 """
 
-@app.route('/')
+@app.route("/")
 def index():
+    encrypted_text=''
     return form.format('','')
+
 @app.route('/',methods=['POST'])
 def encrypt():
     rot=request.form['rot']
@@ -53,7 +57,9 @@ def encrypt():
         return form.format('Error in rotation value','')
     encrypted_text=rotate_string(text,int(rot))
     return form.format('',encrypted_text)
-
-
 if __name__=='__main__':
-    app.run(debug=True)
+
+    app.run(debug=True)    
+
+
+
