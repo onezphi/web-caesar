9e1ff01ff731f60810ef2b9a1835f001558c1262
Franklin <magolin.franklin@gmail.com>
2018-10-30 15:22:02 -0500

    Finish web-caesar

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, request
-from caesar1 import encrypt
+from caesar import rotate_string
 
 
-app = Flask(__name__)
+app=Flask(__name__)
 app.config['DEBUG'] = True
-
 
 form = """
 <!DOCTYPE html>
@@ -19,38 +18,59 @@ form = """
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
+
+
             }}
             textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
             }}
+
         </style>
     </head>
     <body>
-        <form method='POST'>
-            <label>Rotate by:</label>
-            <input type="text" name="rot" value="0" />
-            <textarea name="text">{0}</textarea>
-            <input type="submit" value="Submit Query" />            
+    <div class='layout'>
+      #<!-- create your form here -->
+      #form uses the post method
+      <form methods="post">
+      <label for="rot">Rotate by:</label>
+      #label on the input element
+            <input type= "text" name = "rot" value = "0">
+            
+            #input element above has a default value of 0
+            #per assignment instructions
+        
+        <textarea type = "text" name = "text">{0}</textarea>
+        #second input per the instrux for the body of HTML string
+        <input type = "submit">
         </form>
+        </div>
     
+
+
     </body>
 </html>
+
+
 """
-
-
+#wrapped and unwrapped h1 tags to test
+#processing the form
 @app.route("/")
 def index():
-    return form.format('')
+    encrypted_text= ''
+    return form.format(",")
+
+@app.route('/',methods=['POST'])
+def encrypt():
+    rot=request.form['rot']
+    text=request.form['text']
+    if ' ' in rot or not rot.isdigit():
+        return form.format('Error in rotation value','')
+    encrypted_text=rotate_string(text,int(rot))
+    return form.format('',encrypted_text)
+if __name__=='__main__':
+
+    app.run(debug=True)    
 
 
-@app.route("/", methods=['POST'])
-def encrypt2():
-    rot = int(request.form['rot'])
-    text = request.form['text']
-    #e_msg = rotate_string(text, rot)
-    return form.format(encrypt(text, rot))
-
-
-app.run()
